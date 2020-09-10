a95bff4e4ada7440d86f88d28dfcc6cf40a94993
rezoKom <rezokomakhidze1@gmail.com>
2020-09-10 16:23:45 +0400

    added logic for displaying all articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,9 @@
 class ArticlesController < ApplicationController
+    def index
+        @articles = Article.all
+    end
+
     def show
         @article = Article.find(params[:id])
-        @param = p params
     end
 end
