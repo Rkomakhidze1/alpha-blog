af1df7e4a1d2b04e84577799b41dfedc317a947e
rezoKom <rezokomakhidze1@gmail.com>
2020-09-10 17:19:42 +0400

    added logic for creating articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,13 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+
+    def new
+    end
+
+    def create
+        @new_article = Article.new(title: params[:title], description: params[:description])
+        @new_article.save
+        render json: {article: @new_article}
+    end
 end
