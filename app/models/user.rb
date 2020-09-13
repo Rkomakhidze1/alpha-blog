026301ab7eed38566c057ae2bdc6c02edfbca630
rezoKom <rezokomakhidze1@gmail.com>
2020-09-13 17:00:55 +0400

    added further validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ApplicationRecord
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-
+    has_many :articles
+    before_save {self.email = email.downcase}
+    
     validates :email, presence: true, 
-                        uniqueness: true, 
+                        uniqueness: {case_sensitive: false}, 
                         format: {with: VALID_EMAIL_REGEX}
     validates :username, presence: true, 
                             uniqueness: {case_sensitive: false}
-    has_many :articles
+    
 end
