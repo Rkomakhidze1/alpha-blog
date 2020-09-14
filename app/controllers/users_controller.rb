03e0b500241ba8e6993095dae7fadd1f5163bfcd
rezoKom <rezokomakhidze1@gmail.com>
2020-09-14 11:20:48 +0400

    added basig pagination for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,21 @@
 class UsersController < ApplicationController
     def index
-        users = User.all
+        users = User.paginate(page: params[:page], per_page: 3)
         render json: {data: users}, status: :ok
     end
 
     def create
-        user = User.new(email: params[:email], username: params[:username], password:[params[:password]])
+        user = User.new(user_params)
         if user.save
             render json: {data: user}, status: :ok
         else
             render json: {errors: user.errors.full_messages}, status: :bad_request
         end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password_digest, :username)
     end
 end
