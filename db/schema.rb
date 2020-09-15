b8181643baa408884edeece4a2b4acfd3d852662
rezoKom <rezokomakhidze1@gmail.com>
2020-09-15 20:51:42 +0400

    added admin field to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_15_081222) do
+ActiveRecord::Schema.define(version: 2020_09_15_164730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_09_15_081222) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "tokens", default: [], array: true
+    t.boolean "admin", default: false
   end
 
 end
